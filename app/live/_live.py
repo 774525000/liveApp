c6a4c159c3666d784ffe733e54a6e3464124147a
hehe <774525000@qq.com>
2021-06-04 14:51:01 +0800

    模拟登录

--- app/live/_live.py
+++ app/live/_live.py
@@ -15,9 +15,9 @@ class QueryDetail(Spider):
             'x-sgext': 'JAGzpns4XUETvnffH/qaH6aCloKWgoWBk4CQhYWGkYGFkJeFlIGRhJeBkIQ=',
             'x-social-attr': '3',
             'x-sign': 'azYBCM003xAALXU6R87IzThlikH2/VU9crHvq3KjuhQk6QU8u1nG8RzHBRrpWQ4g+piYG4JcOXOeZ2E5JQwxidR53Kw1TXU9dU11PX',
-            'x-sid': '1b5bc95e650d265b1d8159b38b544c77',
+            'x-sid': user_info['x-sid'],
             'A-SLIDER-Q': 'appKey=21646297&ver=1622733009879',
-            'x-uid': '2100521411',
+            'x-uid': user_info['x-uid'],
             'x-nettype': 'WIFI',
             'x-pv': '6.3',
             'x-nq': 'WIFI',
@@ -30,7 +30,7 @@ class QueryDetail(Spider):
             'x-biz-type': 'live',
             'cache-control': 'no-cache',
             'x-t': f'{ctime.seconds}',
-            'Cookie': 'thw=cn; tfstk=clRVBVxEQszNaV1sps1alLhHblqACjBl3R7fnLuz470x3b0FjE5m7lQSD4jNfRhOn; l=eBPkPikcjnnrHtT-BO5ZPurza7SF2IRZhCVzaNbMiInca6Lh_FM3FNCCPoFX8dtYwt1FbetylIClFdLHR3sdvxDDBUy83k4i32C..; unb=2100521411; sn=; uc3=vt3=F8dCuw76qoBME0VRJPk%3D&id2=UUkO1Kv%2FeCMlYQ%3D%3D&nk2=sym3Yze86A%2FjQSg%3D&lg2=VT5L2FSpMGV7TQ%3D%3D; uc1=cookie21=UtASsssmeW6lpyd%2BB%2B3t&cookie14=Uoe2z%2BTLhW13lw%3D%3D&cookie15=V32FPkk%2Fw0dUvg%3D%3D&existShop=false; csg=bae1b678; lgc=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; t=a2b917c50bd04e33a3a3f98c9b4961df; cookie17=UUkO1Kv%2FeCMlYQ%3D%3D; dnk=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; skt=2f3d81f56f5c16b0; munb=2100521411; cookie2=1b5bc95e650d265b1d8159b38b544c77; uc4=id4=0%40U2uCu3NZb8FJne85RaKSrX7%2FPDS8&nk4=0%40sVYXnVU9W%2B32DLENh4iu5qxpUlckKQ%3D%3D; tracknick=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; _cc_=VFC%2FuZ9ajQ%3D%3D; ti=1568860058617%40taobao_android_9.25.0; sg=21d; _l_g_=Ug%3D%3D; _nk_=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; cookie1=AiBidlwPGZHvoyjWBBi2DYD0Yaidkhe6VY8HPgbAEgU%3D; _tb_token_=ee4b8b383bde; sgcookie=W100z3Cb7qXZSW%2FJp98oc1goxkI6uHOzvW7PndgX0mkI8t%2FOiKraPhPFdoK6pfwGOCy0yTfOMbZakUDhOfEWNXN2rMOuvQxkiyjK2%2F3MzNg7ZOU%3D; imewweoriw=3%2FqcKUC8ML9xucTzN5%2B2W6LA9%2BxNQdXdHyjfcgTjBFU%3D; WAPFDFDTGFG=%2B4cMKKP%2B8PI%2BuUTJFJmnP9vVxdsYhSE6jeU%3D; _w_tb_nick=%E5%B0%8F%E7%B3%AF%E5%9B%A2%E5%AD%90712; ockeqeudmj=r4pchfM%3D; cna=FAwyGVKFSywCAXPFJohibZcS; isg=AhUVQH1-2e2Nnv0Byz_8iFumLxPPEskkH_fZ-pe60Qzb7jXgX2LZ9COuysgn',
+            'Cookie': user_info['Cookie'],
             'x-bx-version': '6.5.27',
             'f-refer': 'mtop',
             'x-extdata': 'openappkey=DEFAULT_AUTH',
@@ -46,7 +46,7 @@ class QueryDetail(Spider):
             'x-appkey': '21646297',
             'x-page-url': 'http://h5.m.taobao.com/taolive/video.html',
             'x-page-name': 'com.taobao.taolive.room.TaoLiveVideoActivity',
-            'x-devid': 'Atzi2cGlhQSAlW_BJjJULENjeY88Cyak5-o8mmUXCIP6',
+            'x-devid': user_info['x-devid'],
             'user-agent': 'MTOPSDK/3.1.1.7 (Android;8.1.0;LGE;Nexus 5X)',
             'Host': 'guide-acs.m.taobao.com',
             'Accept-Encoding': 'gzip',
@@ -57,7 +57,7 @@ class QueryDetail(Spider):
             "useLiveFandom": "true",
             "extendJson": "{\"guardAnchorSwitch\":true,\"version\":\"202003\"}",
             "ignoreH265": "false",
-            "liveId": "311734092779",
+            "liveId": "313467923770",
             "transParams": ""
         }
 
@@ -77,9 +77,9 @@ class Subscribe(Spider):
             'x-sgext': 'JAHsRZhnvh7w4ZSA/KV5QEXddd113WbecN9z2mbZcttmz3Tad95y23Tec9s=',
             'x-social-attr': '3',
             'x-sign': 'azYBCM003xAAIAHfWt8r3KrqLtb1wCHQBlybRgZOzvlQBHHRz7SyHGgqcfedtHrNjnXs9vaxTZ7qihXUUeFFZKCUqEFBQAHQAUAB0A',
-            'x-sid': '1b5bc95e650d265b1d8159b38b544c77',
+            'x-sid': user_info['x-sid'],
             'A-SLIDER-Q': 'appKey=21646297&ver=1622733009879',
-            'x-uid': '2100521411',
+            'x-uid': user_info['x-uid'],
             'x-nettype': 'WIFI',
             'x-pv': '6.3',
             'x-nq': 'WIFI',
@@ -90,7 +90,7 @@ class Subscribe(Spider):
             'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
             'cache-control': 'no-cache',
             'x-t': f'{ctime.seconds}',
-            'Cookie': 'thw=cn; tfstk=clRVBVxEQszNaV1sps1alLhHblqACjBl3R7fnLuz470x3b0FjE5m7lQSD4jNfRhOn; l=eBPkPikcjnnrHtT-BO5ZPurza7SF2IRZhCVzaNbMiInca6Lh_FM3FNCCPoFX8dtYwt1FbetylIClFdLHR3sdvxDDBUy83k4i32C..; unb=2100521411; sn=; uc3=vt3=F8dCuw76qoBME0VRJPk%3D&id2=UUkO1Kv%2FeCMlYQ%3D%3D&nk2=sym3Yze86A%2FjQSg%3D&lg2=VT5L2FSpMGV7TQ%3D%3D; uc1=cookie21=UtASsssmeW6lpyd%2BB%2B3t&cookie14=Uoe2z%2BTLhW13lw%3D%3D&cookie15=V32FPkk%2Fw0dUvg%3D%3D&existShop=false; csg=bae1b678; lgc=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; t=a2b917c50bd04e33a3a3f98c9b4961df; cookie17=UUkO1Kv%2FeCMlYQ%3D%3D; dnk=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; skt=2f3d81f56f5c16b0; munb=2100521411; cookie2=1b5bc95e650d265b1d8159b38b544c77; uc4=id4=0%40U2uCu3NZb8FJne85RaKSrX7%2FPDS8&nk4=0%40sVYXnVU9W%2B32DLENh4iu5qxpUlckKQ%3D%3D; tracknick=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; _cc_=VFC%2FuZ9ajQ%3D%3D; ti=1568860058617%40taobao_android_9.25.0; sg=21d; _l_g_=Ug%3D%3D; _nk_=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; cookie1=AiBidlwPGZHvoyjWBBi2DYD0Yaidkhe6VY8HPgbAEgU%3D; _tb_token_=ee4b8b383bde; sgcookie=W100z3Cb7qXZSW%2FJp98oc1goxkI6uHOzvW7PndgX0mkI8t%2FOiKraPhPFdoK6pfwGOCy0yTfOMbZakUDhOfEWNXN2rMOuvQxkiyjK2%2F3MzNg7ZOU%3D; imewweoriw=3%2FqcKUC8ML9xucTzN5%2B2W6LA9%2BxNQdXdHyjfcgTjBFU%3D; WAPFDFDTGFG=%2B4cMKKP%2B8PI%2BuUTJFJmnP9vVxdsYhSE6jeU%3D; _w_tb_nick=%E5%B0%8F%E7%B3%AF%E5%9B%A2%E5%AD%90712; ockeqeudmj=r4pchfM%3D; cna=FAwyGVKFSywCAXPFJohibZcS; isg=AhUVQH1-2e2Nnv0Byz_8iFumLxPPEskkH_fZ-pe60Qzb7jXgX2LZ9COuysgn',
+            'Cookie': user_info['Cookie'],
             'x-bx-version': '6.5.27',
             'f-refer': 'mtop',
             'x-extdata': 'openappkey=DEFAULT_AUTH',
@@ -105,7 +105,7 @@ class Subscribe(Spider):
             'x-appkey': '21646297',
             'x-page-url': 'http://h5.m.taobao.com/taolive/video.html',
             'x-page-name': 'com.taobao.taolive.room.TaoLiveVideoActivity',
-            'x-devid': 'Atzi2cGlhQSAlW_BJjJULENjeY88Cyak5-o8mmUXCIP6',
+            'x-devid': user_info['x-devid'],
             'user-agent': 'MTOPSDK/3.1.1.7 (Android;8.1.0;LGE;Nexus 5X)',
             'Host': 'guide-acs.m.taobao.com',
             'Accept-Encoding': 'gzip',
@@ -116,13 +116,13 @@ class Subscribe(Spider):
             "appKey": "21646297",
             "ext": f"{ctime.millis}",
             "from": "小糯团子712",
-            "id": "2100521411",
+            "id": user_info['x-uid'],
             "namespace": 1,
             "role": 3,
             "sdkVersion": "0.3.0",
             "tag": "tb",
             "timestamp": ctime.millis,
-            "topic": "a2d99dae-8e0b-4879-857b-e824f016c351",
+            "topic": topic,
             "utdId": "YKnJdlCwF6oDACfhFIu95LNr"
         }
 
@@ -138,9 +138,9 @@ class Report(Spider):
             'x-sgext': 'JAG8P+I3xE6Kse7QhvUDED+ND40PjRyOCo8JihyJB44cnw6KDY4Iiw6OCYs=',
             'x-social-attr': '3',
             'x-sign': 'azYBCM003xAAK8tYybQ7Sx8K9MpPO+tbzNdRzczFBHKaj7taBT94l6Khu3xXP7BGRP4mfTw6hxUgAd9fm2qP72ofYsqKG8tbyhvLW8',
-            'x-sid': '1b5bc95e650d265b1d8159b38b544c77',
+            'x-sid': user_info['x-sid'],
             'A-SLIDER-Q': 'appKey=21646297&ver=1622733009879',
-            'x-uid': '2100521411',
+            'x-uid': user_info['x-uid'],
             'x-nettype': 'WIFI',
             'x-pv': '6.3',
             'x-nq': 'WIFI',
@@ -152,7 +152,7 @@ class Report(Spider):
             'Content-Length': '1071',
             'x-t': f'{ctime.seconds}',
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-            'Cookie': 'thw=cn; tfstk=clRVBVxEQszNaV1sps1alLhHblqACjBl3R7fnLuz470x3b0FjE5m7lQSD4jNfRhOn; l=eBPkPikcjnnrHtT-BO5ZPurza7SF2IRZhCVzaNbMiInca6Lh_FM3FNCCPoFX8dtYwt1FbetylIClFdLHR3sdvxDDBUy83k4i32C..; unb=2100521411; sn=; uc3=vt3=F8dCuw76qoBME0VRJPk%3D&id2=UUkO1Kv%2FeCMlYQ%3D%3D&nk2=sym3Yze86A%2FjQSg%3D&lg2=VT5L2FSpMGV7TQ%3D%3D; uc1=cookie21=UtASsssmeW6lpyd%2BB%2B3t&cookie14=Uoe2z%2BTLhW13lw%3D%3D&cookie15=V32FPkk%2Fw0dUvg%3D%3D&existShop=false; csg=bae1b678; lgc=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; t=a2b917c50bd04e33a3a3f98c9b4961df; cookie17=UUkO1Kv%2FeCMlYQ%3D%3D; dnk=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; skt=2f3d81f56f5c16b0; munb=2100521411; cookie2=1b5bc95e650d265b1d8159b38b544c77; uc4=id4=0%40U2uCu3NZb8FJne85RaKSrX7%2FPDS8&nk4=0%40sVYXnVU9W%2B32DLENh4iu5qxpUlckKQ%3D%3D; tracknick=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; _cc_=VFC%2FuZ9ajQ%3D%3D; ti=1568860058617%40taobao_android_9.25.0; sg=21d; _l_g_=Ug%3D%3D; _nk_=%5Cu5C0F%5Cu7CEF%5Cu56E2%5Cu5B50712; cookie1=AiBidlwPGZHvoyjWBBi2DYD0Yaidkhe6VY8HPgbAEgU%3D; _tb_token_=ee4b8b383bde; sgcookie=W100z3Cb7qXZSW%2FJp98oc1goxkI6uHOzvW7PndgX0mkI8t%2FOiKraPhPFdoK6pfwGOCy0yTfOMbZakUDhOfEWNXN2rMOuvQxkiyjK2%2F3MzNg7ZOU%3D; imewweoriw=3%2FqcKUC8ML9xucTzN5%2B2W6LA9%2BxNQdXdHyjfcgTjBFU%3D; WAPFDFDTGFG=%2B4cMKKP%2B8PI%2BuUTJFJmnP9vVxdsYhSE6jeU%3D; _w_tb_nick=%E5%B0%8F%E7%B3%AF%E5%9B%A2%E5%AD%90712; ockeqeudmj=r4pchfM%3D; cna=FAwyGVKFSywCAXPFJohibZcS; isg=AhUVQH1-2e2Nnv0Byz_8iFumLxPPEskkH_fZ-pe60Qzb7jXgX2LZ9COuysgn',
+            'Cookie': user_info['Cookie'],
             'x-bx-version': '6.5.27',
             'f-refer': 'mtop',
             'x-extdata': 'openappkey=DEFAULT_AUTH',
@@ -167,7 +167,7 @@ class Report(Spider):
             'x-appkey': '21646297',
             'x-page-url': 'http://h5.m.taobao.com/taolive/video.html',
             'x-page-name': 'com.taobao.taolive.room.TaoLiveVideoActivity',
-            'x-devid': 'Atzi2cGlhQSAlW_BJjJULENjeY88Cyak5-o8mmUXCIP6',
+            'x-devid': user_info['x-devid'],
             'user-agent': 'MTOPSDK/3.1.1.7 (Android;8.1.0;LGE;Nexus 5X)',
             'Host': 'guide-acs.m.taobao.com',
             'Accept-Encoding': 'gzip',
@@ -176,8 +176,8 @@ class Report(Spider):
 
         self.data = {
             "anchorId": "790956988",
-            "liveEventsJson": "[{\"accountId\":\"790956988\",\"action\":\"enter\",\"count\":1,\"extendJson\":\"{\\\"entrySource\\\":\\\"null\\\",\\\"fansLevel\\\":\\\"0\\\",\\\"liveSource\\\":\\\"null\\\",\\\"roomStatus\\\":\\\"1\\\",\\\"serverParams\\\":\\\"null\\\",\\\"sjsdItemId\\\":\\\"null\\\",\\\"timeMovingItemId\\\":\\\"null\\\",\\\"timeShift\\\":\\\"false\\\",\\\"timeShiftEntry\\\":\\\"0\\\"}\",\"feedId\":\"311734092779\",\"scene\":\"taobaolive\",\"timestamp\":\"%s\",\"type\":\"0\"}]" % ctime.millis,
-            "liveId": "311734092779"
+            "liveEventsJson": "[{\"accountId\":\"790956988\",\"action\":\"enter\",\"count\":1,\"extendJson\":\"{\\\"entrySource\\\":\\\"null\\\",\\\"fansLevel\\\":\\\"0\\\",\\\"liveSource\\\":\\\"null\\\",\\\"roomStatus\\\":\\\"1\\\",\\\"serverParams\\\":\\\"null\\\",\\\"sjsdItemId\\\":\\\"null\\\",\\\"timeMovingItemId\\\":\\\"null\\\",\\\"timeShift\\\":\\\"false\\\",\\\"timeShiftEntry\\\":\\\"0\\\"}\",\"feedId\":\"313467923770\",\"scene\":\"taobaolive\",\"timestamp\":\"%s\",\"type\":\"0\"}]" % ctime.millis,
+            "liveId": "313467923770"
         }
 
 
